6dee2e8cd998f94fe88535bb7f0af5425a0fcfdf
Fahim Ahmed <fahim@Ares.local>
2018-12-26 11:45:45 -0500

    done

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -1,6 +1,23 @@
 package math.problems;
 
 public class MakePyramid {
+    public static void printTriangle ( int n)
+    {
+        int k = 4 * n -4;
+        for (int i=0; i<n; i++)
+        {
+            for (int j = 0; j<k; j++)
+            {
+                System.out.print(" ");
+            }
+            k = k-1;
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
 
         /*   Implement a large Pyramid of stars in the screen with java.
@@ -13,7 +30,7 @@ public class MakePyramid {
                          * * * * * *
 
         */
-
-
+        int n = 4;
+        printTriangle(n);
         }
   }
